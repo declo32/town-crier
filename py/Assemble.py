2bdfb4ec43e67bf60d35324ea3807a8d6cc60c1a
Declan O'Neill <declanone32@gmail.com>
2017-03-15 09:30:45 -0400

    String formatting changes, changes to css

--- py/Assemble.py
+++ py/Assemble.py
@@ -1,13 +1,13 @@
-with open("../html/Skeleton.html", "rb", errors="replace") as file:
+with open("../html/Skeleton.html", "r", errors="ignore") as file:
     skeleton = file.read()
 
-with open("../html/from-school.html", "rb", errors="replace") as file:
+with open("../html/from-school.html", "r", errors="ignore") as file:
     from_school = file.read()
 
-with open("../html/from-twitter.html", "rb", errors="replace") as file:
+with open("../html/from-twitter.html", "r", errors="ignore") as file:
     from_twitter = file.read()
 
-with open("../SlideShow.html", "wb") as file:
+with open("../SlideShow.html", "w") as file:
     file.write(
         # I don't like this
         skeleton % (from_school, from_twitter)
