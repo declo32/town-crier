0e5015dd7958172b6dfc52577a90d720d245e0a1
root <declo32@users.noreply.github.com>
2017-04-03 13:28:44 +0000

    Added command line arguments and timeout

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# Parse arguments
+
+TIMEOUT=$(expr 60*60*8)  # Defaults to 8 hours
+for i in "$@"; do
+	case $i in
+		--time-out=*)
+		TIMEOUT="${i#*=}"
+		shift
+		;;
+		
+		*)
+		echo "$i isn't a thing"
+	esac
+done
+
+echo "TIMEOUT = ${TIMEOUT}"
+
+# Get info
+
 cd py
 
 # Order is important
@@ -9,13 +28,13 @@ declare -a files=(
     "Assemble.py"
 )
 
-for file in "${files[@]}"; do
-    echo "Running $file"
-    sudo python3 "$file"
+for file in "${files[@]}"; do  # files[@]
+    echo "Running ${file}"
+    sudo python3 "${file}"
 done
 
 # Launch display
 
 cd ..
 
-epiphany SlideShow.html & PID=$!; sleep 20; kill $PID
+epiphany SlideShow.html & PID=$!; sleep $TIMEOUT; kill $PID
