deca49b1eef57c3614a2d05984015acbbe32fc4f
root <declo32@users.noreply.github.com>
2017-04-11 15:01:54 +0000

    Made display better again

--- run.sh
+++ run.sh
@@ -44,10 +44,10 @@ fi
 
 # Launch display
 
-unclutter -display :0 -noevents -grab & # PID1=$!  # This hides the mouse
+unclutter -display :0 -noevents -grab & PID1=$!  # This hides the mouse
 epiphany SlideShow.html & PID2=$!
 sleep 10; xte "key F11" -x:0  # This literally to pushes the F11 key
 sleep $DISPLAYTIME
 echo "I'm about to kill the processes known as ${PID1} and ${PID2}"
 kill $PID1
-# kill $PID2
+kill $PID2
