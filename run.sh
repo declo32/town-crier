bfe9b881982bb30556e236eead8416d3b3267292
Declan O'Neill <declanone32@gmail.com>
2017-04-13 08:43:33 -0400

    Made it better

--- run.sh
+++ run.sh
@@ -37,14 +37,14 @@ echo "SHUTDOWN    = ${SHUTDOWN}"
 
 # Get info
 
-if [ ! $USEOLD = true ]; then
+if [ ! ${USEOLD} = true ]; then
 	cd py
 	
 	declare -a FILES=("Announcement-Scrape.py" "Twitter-Scrape.py" "Assemble.py")
 	for FILENAME in "${FILES[@]}"
 	do
-		echo "Running $FILENAME"
-		python3 $FILENAME
+		echo "Running ${FILENAME}"
+		python3 ${FILENAME}
 	done
 	
 	cd ..
@@ -55,13 +55,13 @@ fi
 unclutter -display :0 -noevents -grab & PID1=$!  # This hides the mouse
 epiphany SlideShow.html & PID2=$!
 sleep 20; xte "key F11" -x:0  # This literally to pushes the F11 key
-sleep $DISPLAYTIME
+sleep ${DISPLAYTIME}
 echo "I'm about to kill the processes known as ${PID1} and ${PID2}"
-kill $PID1
-kill $PID2
+kill ${PID1}
+kill ${PID2}
 
 # Shutdown
 
-if [ $SHUTDOWN = true ]; then
+if [ ${SHUTDOWN} = true ]; then
 	shutdown --poweroff now
 fi
