42a6c9f19951f69f5a43b64e5fcbf1e2cdf4e0e1
the thing that's supposed to run this <declo32@users.noreply.github.com>
2017-04-25 14:58:43 +0000

    Removed unnecessary feature

--- run.sh
+++ run.sh
@@ -56,13 +56,11 @@ fi
 
 # Launch display
 
-unclutter -display :0 -noevents -grab & PID1=$!  # This hides the mouse
-epiphany SlideShow.html & PID2=$!
+epiphany SlideShow.html & PID=$!
 sleep 20; xte "key F11" -x:0  # This literally to pushes the F11 key
 sleep ${DISPLAYTIME}
-echo "I'm about to kill the processes known as ${PID1} and ${PID2}"
-kill ${PID1}
-kill ${PID2}
+echo "I'm about to kill the process known as ${PID}"
+kill ${PID}
 
 # Shutdown
 
